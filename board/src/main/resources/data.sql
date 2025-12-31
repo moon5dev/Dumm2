62ce67e5fac7fdea5e47339410595329528383a1
moon5dev <jitaemoon@moon5.dev>
2025-12-31 14:12:03 +0900

    feat: Add data for test account and related yaml setting

---
+++ board/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USERS (username, password, name, role, is_deleted, created_date)
+VALUES ('admin', '$2a$12$MzUx2NbTJspRWLfpI.6sq.yL2GgLNEw1ExliFEt7xAkoSWHScMHJ2', 'admin', 'ADMIN', false, NOW());
