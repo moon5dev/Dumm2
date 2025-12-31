369cd4a9cae473ad0e472d122629a3b7ea0c4bf2
moon5dev <jitaemoon@moon5.dev>
2025-12-31 14:11:33 +0900

    feat: Implement security related classes

---
+++ board/src/main/java/dev/moon5/board/controller/LoginController.java
@@ -0,0 +1,18 @@
+package dev.moon5.board.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        log.info("LoginController :: login()");
+
+        return "login";
+    }
+
+}
