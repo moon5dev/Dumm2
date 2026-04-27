1336ff09632c417644d9f61a0eefd6c22872c1d6
moon5dev <jitaemoon@moon5.dev>
2026-04-27 22:01:26 +0900

    feat: Add project setup

---
+++ sap-connector/src/main/java/dev/moon5/sapconnector/SapConnectorApplication.java
@@ -0,0 +1,13 @@
+package dev.moon5.sapconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapConnectorApplication.class, args);
+	}
+
+}
